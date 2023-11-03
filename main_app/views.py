d21273ff9eee2419bca49f5886df54e90ac8e24b
gryantgilgan <bryantgilgan@gmail.com>
2023-11-02 19:49:27 -0700

    adds partner views

--- main_app/views.py
+++ main_app/views.py
@@ -21,9 +21,11 @@ def pops_index(request):
 def pops_detail(request, pop_id):
     pop = Pop.objects.get(id=pop_id)
     id_list = pop.partners.all().values_list('id')
+    partners_pop_doesnt_have = Partner.objects.exclude(id_in=id_list)
     logging_form = LoggingForm()
     return render(request, 'pops/detail.html', {
-        'pop': pop, 'logging_form': logging_form
+        'pop': pop, 'logging_form': logging_form,
+        'partners': partners_pop_doesnt_have
     })
 
 class PopCreate(CreateView):
@@ -44,4 +46,30 @@ def add_logging(request, pop_id):
         new_logging = form.save(commit=False)
         new_logging.pop_id = pop_id
         new_logging.save()
+    return redirect('detail', pop_id=pop_id)
+
+class PartnerList(ListView):
+    model = Partner
+
+class PartnerDetail(DetailView):
+    model = Partner
+
+class PartnerCreate(CreateView):
+    model = Partner
+    fields = '__all__'
+
+class PartnerUpdate(UpdateView):
+    model = Partner
+    fields = ['name', 'item_no']
+
+class PartnerDelete(DeleteView):
+    model = Partner
+    success_url = '/partners'
+
+def assoc_partner(request, pop_id, partner_id):
+    Pop.objects.get(id=pop_id).partners.add(partner_id)
+    return redirect('detail', pop_id=pop_id)
+
+def unassoc_partner(request, pop_id, partner_id):
+    Pop.objects.get(id=pop_id).partners.remove(partner_id)
     return redirect('detail', pop_id=pop_id)
